dd864e2c8b4c7e82f1eae516cb297eb5c9402940
andreprogramador01 <andreprogramador01@gmail.com>
2022-12-02 21:36:29 -0300

    flashcard mudando a cor de acordo com a escolha do usuario

--- src/components/Pergunta.js
+++ src/components/Pergunta.js
@@ -1,14 +1,38 @@
+import { useState } from 'react'
+import imgVirar from '../img/seta_virar.png'
+
 export default function Pergunta(props){
+    const [viraPergunta, setViraPergunta] = useState(false)
+
+    function virarPergunta(){
+        if(viraPergunta === false){
+            setViraPergunta(true);
+        }else{
+            setViraPergunta(false);
+        }
+
+    }
     
     return(
         <>
-            <li className="pergunta-aberta">{props.questao}
-            <div className="container-botoes">
-                <button className="nao-lembrei">Não lembrei</button>
-                <button className="quase-nao">Quase não lembrei</button>
-                <button className="zap">Zap!</button>
-            </div>
-            </li>
+            {
+               !viraPergunta ? (
+                <li className="pergunta-aberta">{props.questao}
+                    <img src={imgVirar} onClick={virarPergunta}></img>
+                </li>
+                ):(
+                <li className="pergunta-aberta">{props.resposta}
+                    <div className="container-botoes">
+                        <button onClick={()=>props.riscarTexto('#FF3030',props.datakey)} className="nao-lembrei">Não lembrei</button>
+                        <button onClick={()=>props.riscarTexto('#FF922E',props.datakey)} className="quase-nao">Quase não lembrei</button>
+                        <button onClick={()=>props.riscarTexto('#2FBE34',props.datakey)} className="zap">Zap!</button>
+                    </div>
+                </li>
+                ) 
+            }
+            
+            {/**/}
+            
             
         </>
     )
--- src/components/Perguntas.js
+++ src/components/Perguntas.js
@@ -4,6 +4,9 @@ import { useState } from "react"
 
 export default function Perguntas(props){
     const [exibirCard, setExibirCard] = useState([])
+    const [estiloTexto, setEstiloTexto] = useState('')
+    const [ultimaChave, setUltimaChave] = useState('')
+    const [cardsClicados, setCardsClicados] = useState([{}])
     const cards = [
         { question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
         { question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
@@ -14,17 +17,39 @@ export default function Perguntas(props){
         { question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
         { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
     ]
-
+    function riscarTexto(cor, key){
+    let novoCard = [...cardsClicados, {key: key, cor: cor}]
+    
+    setCardsClicados(novoCard)
+    // console.log(novoCard.filter(card=>card.key === "O que é JSX?"));
+    setExibirCard(exibirCard.filter(item => item !== key))
+    
+    }
     function virarCard(posicao){
         setExibirCard([...exibirCard, posicao])
     }
+    function filtraCard(questao){
+        let filtrada = cardsClicados.filter(card=>card.key === questao)
+        return filtrada[0] ? filtrada[0].cor : '';
+        
+    }
     
     return(
-        <ul className="">
+        <ul className="Perguntas">
                 {cards.map( (c,index) => (
-                    exibirCard.includes(index) ? 
-                        <Pergunta key={c.question} questao={c.question}/> :
-                        <li key={c.question} className="pergunta-fechada">Pergunta {index+1} <img onClick={() => virarCard(index)} src={setaPlay} /></li>)
+                    exibirCard.includes(c.question) ? 
+                        <Pergunta   datakey={c.question}
+                                    key={c.question} 
+                                    questao={c.question} 
+                                    resposta={c.answer}
+                                    riscarTexto={riscarTexto}
+                                    /> 
+                        :
+                        <li 
+                            key={c.question} 
+                            style={ {color:  filtraCard(c.question) } }
+                            datakey={c.question}
+                            className="pergunta-fechada">Pergunta {index+1} <img onClick={() => virarCard(c.question)} src={setaPlay} /></li>)
                 )
                 }
             
